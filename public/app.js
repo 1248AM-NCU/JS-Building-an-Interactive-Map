3d5ee7f0bee0603ddf513126afef238e1fbc656e
TheLegendary1248 <1248alex@gmail.com>
2022-04-06 20:34:12 -0400

    Final Result

--- public/app.js
+++ public/app.js
@@ -1,17 +1,24 @@
 // create map
+var map = L.map('map').setView([51.505, -0.09], 13);
 
-
-// add openstreetmap tiles
-
-
-// create and main add geolocation marker
-
+// Add OpenStreetMap tiles:
+L.tileLayer('https://.tile.openstreetmap.org///.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: '15',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(map)
 
 // draw the 2nd arrondissement
-
+var marker = L.marker([51.5, -0.09]).addTo(map);
 
 // create red pin marker
-
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
 
 // metro station markers
 
